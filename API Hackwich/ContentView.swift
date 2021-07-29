eada08fbc827c5aa1bfcca9a7fc54884f6135589
Amelia <atan@ucls.uchicago.edu>
2021-07-29 11:28:45 -0500

    Got API key

--- API Hackwich/ContentView.swift
+++ API Hackwich/ContentView.swift
@@ -27,12 +27,7 @@ struct ContentView: View {
         
     }
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=2ebab9e138msh9178605bc56f667p1873edjsn7b521d2c15f8"
     }
 }
 
