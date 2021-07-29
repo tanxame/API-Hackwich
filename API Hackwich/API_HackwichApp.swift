2642152f2af7e8d6ba4a059abee37207c8009b23
Amelia <atan@ucls.uchicago.edu>
2021-07-29 11:10:42 -0500

    Initial Commit

---
+++ API Hackwich/API_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_HackwichApp.swift
+//  API Hackwich
+//
+//  Created by Franklin Tan on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct API_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
